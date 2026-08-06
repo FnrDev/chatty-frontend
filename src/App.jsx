59988800641ab8b7a4c72b70e12d4936123c5c83
xCuzSkillz <pvpplayer403020@gmail.com>
2026-08-06 11:40:24 +0300

    Declear layout sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
 import { useState } from "react";
 import { Route, Routes } from "react-router";
-import Navbar from "./components/Navbar";
 import SignupPage from "./pages/SignupPage";
-import Homepage from "./pages/Homepage";
 import SignInPage from "./pages/SigninPage";
-import Dashboard from "./pages/Dashboard";
-import { useEffect } from "react";
-import { getCurrentUser, logout } from "./services/authService";
+// import { useEffect } from "react";
+// import { getCurrentUser, logout } from "./services/authService";
 import ProtectedRoute from "./components/ProtectedRoute";
-import { useAuth } from "./context/AuthContext";
+// import { useAuth } from "./context/AuthContext";
+import MainPage from "./pages";
 function App() {
   return (
     <div>
-      <Navbar/>
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/sign-up" element={<SignupPage />} />
-        <Route path="/sign-in" element={<SignInPage />} />
-        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        <Route path="/" element={<MainPage />} />
+        {/* <Route path="/sign-up" element={<SignupPage />} />
+        <Route path="/sign-in" element={<SignInPage />} /> */}
+        {/* <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} /> */}
       </Routes>
     </div>
   );
