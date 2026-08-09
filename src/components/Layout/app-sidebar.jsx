0f383c210991cb04fe2129e6f97785eb4ed5edad
Ahmed <fnr5541@gmail.com>
2026-08-09 15:18:43 +0300

    fix naming of app

--- src/components/Layout/app-sidebar.jsx
+++ src/components/Layout/app-sidebar.jsx
@@ -17,7 +17,7 @@ export function AppSidebar(props) {
       <SidebarHeader className="px-2.5">
         <div className="flex items-center gap-1 mb-1">
           <img src="/logo.svg" width={50} height={50} />
-          <h2 className="text-2xl">Chatti</h2>
+          <h2 className="text-2xl">Chatty</h2>
         </div>
       <TeamSwitcher />
       </SidebarHeader>
