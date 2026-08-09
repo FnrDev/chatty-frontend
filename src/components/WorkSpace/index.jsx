4444c215ff6d4437d5a6681f5b53e7d84ba81a84
xCuzSkillz <pvpplayer403020@gmail.com>
2026-08-09 14:56:32 +0300

    Channels setup

--- src/components/WorkSpace/index.jsx
+++ src/components/WorkSpace/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useParams } from 'react-router'
 
 export default function WorkSpace() {
+
+  const { id } = useParams()
   return (
     <div>
       <h2>This is the chat begning</h2>
