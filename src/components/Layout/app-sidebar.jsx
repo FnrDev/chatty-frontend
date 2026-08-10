b8a0b1d591a0981290ce2cfc79be8757d03b5094
Ahmed <fnr5541@gmail.com>
2026-08-10 17:47:29 +0300

    handle websocket for message received and send_message

--- src/components/Layout/app-sidebar.jsx
+++ src/components/Layout/app-sidebar.jsx
@@ -9,7 +9,7 @@ import CreateChannel from "../Channel/CreateChannel"
 import { useAuth } from "@/context/AuthContext"
 import { useEffect, useState } from "react"
 import api from "@/services/api"
-import { useParams } from "react-router"
+import { Link, useParams } from "react-router"
 import { Hash } from "lucide-react"
 
 export function AppSidebar(props) {
@@ -22,7 +22,7 @@ export function AppSidebar(props) {
 
   async function getChannels() {
    try {
-    const response = await api.get(`/workspace/${id}/channels`)
+    const response = await api.get(`/workspaces/${id}/channels`)
     setChannels(...channels, response.data)
    } catch(err) {
     console.log(err)
@@ -51,18 +51,18 @@ export function AppSidebar(props) {
         <div className="flex flex-col gap-3 mt-5">
           {channels.map((e) => {
           return(
-           <a href={`/workspaces/${id}/${e._id}`}>
+           <Link to={`/workspaces/${id}/${e._id}`}>
              <div className="flex p-2 px-3 flex-col gap-1 bg-sidebar-accent rounded-lg">
               <p className="text-[14px] flex items-center gap-2"><Hash size={15} className="text-white/60" /> {e.name}</p>
             </div>
-           </a>
+           </Link>
           )
         })}
         </div>
       </SidebarContent>
       <SidebarFooter>
         <div className="bg-popover rounded-md flex items-center gap-3 p-3">
-          <img width={40} className="rounded-md" height={40} src="https://i.pinimg.com/1200x/dc/fb/17/dcfb17e9334c906c8a62273ae0a34900.jpg" />
+          <img width={40} className="rounded-md" height={40} src={user.profileImage} />
           <p>{user.username}</p>
         </div>
       </SidebarFooter> 
