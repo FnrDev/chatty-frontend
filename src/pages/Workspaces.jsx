b3501793a0f32be1318316226201e0be42bd2d45
Ahmed <fnr5541@gmail.com>
2026-08-12 15:12:47 +0300

    fix workspace image overlay

--- src/pages/Workspaces.jsx
+++ src/pages/Workspaces.jsx
@@ -126,11 +126,10 @@ function WorkspaceCard({ workspace, isOwner, onOpen }) {
 
   return (
     <Card className="relative w-full pt-0">
-      <div className="pointer-events-none absolute inset-x-0 top-0 z-30 aspect-video bg-black/35" />
       <img
         src={cover}
         alt={`${workspace.name} cover`}
-        className="relative z-20 aspect-video w-full object-cover brightness-60 grayscale dark:brightness-40"
+        className="aspect-video w-full object-cover"
       />
       <CardHeader>
         <CardAction>
